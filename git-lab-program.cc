8c5db3fa411ac97f0322bf58c1bb5c27afb85f68
William <wh994921@ohio.edu>
2023-01-27 14:54:06 -0500

    Commiting this test git file.

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <William Hamilton>
+ *        Date: <Jan 27th, 2023>
+ * Description: This is for Lab 2, question 15.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
